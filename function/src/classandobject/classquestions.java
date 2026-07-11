92a288921665815dc4bfc7b9f68990924f093c22
roshan <roshangawhade25@gmail.com>
2026-07-11 13:01:24 +0530

    push

---
+++ function/src/classandobject/classquestions.java
@@ -0,0 +1,49 @@
+//package classandobject;
+//
+//class A{
+//	int x;
+//	void m() {
+//		System.out.println("hello");
+//	}
+//}
+//public class classquestions {
+//	public static void main(String[] args) {
+//		A a1 = new A();
+//		a1.x = 10;
+//		a1.m();
+//		A b = new A();
+//		b.x = 20;
+//		System.out.println(a1.x);
+//		System.out.println(b.x);
+//		
+//	}
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//package classandobject;
+//class AA{
+//	int x;
+//	String y;
+//	void show() {
+//		System.out.println(x+y);
+//	}
+//}
+//public class classquestions{
+//	public static void main(String[] args) {
+//		AA a1 = new AA();
+//		a1.x = 10;
+//		AA b = new AA();
+//		b.y = "world";
+//		a1.show();
+//		b.show();
+//	}
+//}
---
+++ function/src/function/function1.java
@@ -0,0 +1,20 @@
+package function;
+
+public class function1 {
+	static void rio () {
+		System.out.println("rio:tokyo");
+		return;
+	}
+	static void tokyo () {
+		System.out.println("tokyo :i want money" );
+	}
+	public static void main(String[] args) {
+		rio ();
+		rio ();
+		rio ();
+		tokyo (); 
+	}
+	
+	
+
+}
